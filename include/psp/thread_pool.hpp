4a5e06635fe5a856715900417e0674e225a4cca6
pknowles <pknowles@users.noreply.github.com>
2022-07-17 19:34:11 -0700

    fix thread pool global lock and race on erase()

--- include/psp/thread_pool.hpp
+++ include/psp/thread_pool.hpp
@@ -9,76 +9,105 @@
 
 #include <assert.h>
 #include <condition_variable>
-#include <mutex>
-#include <list>
-#include <thread>
 #include <functional>
+#include <list>
+#include <mutex>
+#include <thread>
 
 namespace psp {
 
-class thread_pool
-{
+class thread_pool {
 public:
     using multitask = std::function<bool()>;
 
-    thread_pool(size_t count)
-    {
-        // All threads start from the beginning of the queue and do one pass through it
-        
+    thread_pool(size_t count) {
+        // All threads start from the beginning of the queue and do one pass
+        // through it
+
         for (size_t i = 0; i < count; ++i)
-            m_threads.emplace_back(&thread_pool::entrypoint, this, m_multitasks.begin());
+            m_threads.emplace_back(&thread_pool::entrypoint, this,
+                                   m_multitasks.begin());
     }
-    ~thread_pool()
-    {
+    ~thread_pool() {
         {
             std::lock_guard<std::mutex> lk(m_multitasksMutex);
             m_running = false;
             m_multitasksCond.notify_all();
         }
-        for (auto& thread : m_threads)
+        for (auto &thread : m_threads)
             thread.join();
     }
 
-    template<class Func>
-    void process(Func&& func)
-    {
+    template <class Func> void process(Func &&func) {
         std::lock_guard<std::mutex> lk(m_multitasksMutex);
+        m_tasksAlive++;
         m_multitasks.emplace_back(func);
         m_multitasksCond.notify_all();
     }
 
 private:
-    void entrypoint(std::list<multitask>::iterator iterator) {
+    struct Task {
+        std::shared_ptr<multitask> func;
+        bool alive{true};
+        Task() {}
+        template <class Func>
+        Task(Func &&func) : func(std::make_shared<multitask>(func)) {}
+    };
+
+    void entrypoint(std::list<Task>::iterator iterator) {
         bool first = true;
         bool taskDone = false;
+        Task task;
         for (;;) {
-            std::unique_lock<std::mutex> lk(m_multitasksMutex);
-            if (!first) {
-                if (taskDone) {
-                    m_multitasks.erase(iterator++);
+            {
+                std::unique_lock<std::mutex> lk(m_multitasksMutex);
+                if (!first) {
+                    if (taskDone && iterator->alive) {
+                        iterator->alive = false;
+                        --m_tasksAlive;
+                    }
+                    if (!iterator->alive && task.func.use_count() == 2) {
+                        // The last user of finished tasks removes it.
+                        // There "thould" (*hopes even harder*) be no dangling
+                        // iterators
+                        m_multitasks.erase(iterator++);
+                    } else {
+                        iterator++;
+                    }
                 } else {
-                    iterator++;
+                    first = false;
                 }
-            } else {
-                first = false;
+
+                // Release our reference to the task so the last thread can
+                // remove it from the list.
+                task.func.reset();
+
+                // Skip over completed tasks. Each of them should have threads
+                // waiting on this lock to come in and erase them.
+                while (iterator != m_multitasks.end() && !iterator->alive)
+                    ++iterator;
+
+                if (iterator == m_multitasks.end()) {
+                    m_multitasksCond.wait(
+                        lk, [&] { return !m_running || m_tasksAlive; });
+                    if (!m_running)
+                        return;
+                    iterator = m_multitasks.begin();
+                    while (!iterator->alive)
+                        ++iterator;
+                }
+                task = *iterator;
             }
-            if (iterator == m_multitasks.end()) {
-                m_multitasksCond.wait(
-                    lk, [&] { return !m_running || !m_multitasks.empty(); });
-                if (!m_running)
-                    return;
-                iterator = m_multitasks.begin();
-            }
-            auto &task = *iterator;
-            taskDone = !task();
+            taskDone = !(*task.func)();
         }
     }
 
-    std::list<multitask> m_multitasks;
+    std::list<Task> m_multitasks;
     std::vector<std::thread> m_threads;
     std::mutex m_multitasksMutex;
     std::condition_variable m_multitasksCond;
     bool m_running{true};
+    size_t m_tasksAlive{0};
 };
 
 } // namespace psp
