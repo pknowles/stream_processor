bd41d7a1247f38efff7c544c6eb3fb9420e32cc6
pknowles <pknowles@users.noreply.github.com>
2022-07-17 23:59:58 -0700

    only compile fuzz.cpp with clang

--- test/src/fuzz.cpp
+++ test/src/fuzz.cpp
@@ -35,5 +35,3 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     //   assert(sum == verify);
     return 0; // Values other than 0 and -1 are reserved for future use.
 }
-
-int main() { return 0; }
